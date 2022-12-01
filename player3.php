32793cbe43ca175513b6e11b45cd04ec0308a24c
QasimSultani <qasimsultani11@gmail.com>
2022-12-01 21:35:20 +0500

    first commit

---
+++ player3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <style type="text/css">
+  	
+ .iframe-container{
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%; 
+  height: 0;
+}
+
+ 	iframe {
+    margin: 240px auto;
+    display: block;
+}
+
+  </style>
+</head>
+<body style="background-color:black;">
+	<div class="iframe-container">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/tlyEvfJU1ws" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</body>
+</html>
